81c38d6134a4a83b198848321712b9d9302a51af
Abigail91 <abigail_abarca@hotmail.com>
2019-10-04 10:19:48 -0600

    Insertion Sort

---
+++ Sorts/InsertionSort.h
@@ -0,0 +1,18 @@
+//
+// Created by abigail on 3/10/19.
+//
+
+#ifndef TAREAEXTRACLASE3_INSERTIONSORT_H
+#define TAREAEXTRACLASE3_INSERTIONSORT_H
+
+
+class InsertionSort {
+    public:
+        void insertionSort(int arr[], int n);
+        void printArray(int arr[], int n);
+
+
+};
+
+
+#endif //TAREAEXTRACLASE3_INSERTIONSORT_H
