2a4be7221b2331b863fafe860fad649c05878b38
victoria <vicvargas99@gmail.como>
2019-10-06 19:33:17 -0600

    BubbleSort

---
+++ Sorts/BubbleSort.h
@@ -0,0 +1,23 @@
+//
+// Created by victoria on 6/10/19.
+//
+
+#ifndef TAREAEXTRACLASEIII_BUBBLESORT_H
+#define TAREAEXTRACLASEIII_BUBBLESORT_H
+
+/**
+ * Declaración de la clase del algoritmo Bubble Sort
+ * Tomado de: https://www.geeksforgeeks.org/bubble-sort/
+ */
+
+    class BubbleSort {
+    public:
+        void bubbleSort(int arr[], int n);
+        void swap(int *xp, int *yp);
+
+    };
+
+
+
+
+#endif //TAREAEXTRACLASEIII_BUBBLESORT_H
